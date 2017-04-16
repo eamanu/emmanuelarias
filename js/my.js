e18829a304f6b0f037a95313828c8ba256975463
eamanu <emmanuelarias30@gmail.com>
2017-04-16 10:50:13 -0300

    finished cv

--- js/my.js
+++ js/my.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
       $(this).find(".skill-bar").animate({
             width:$(this).attr("data")},6000);
           });
-    });
+
+    $(function(){
+          $(".hello").typed({
+            strings:["Ha!","Hi! My man", "Hi! I am Emmanuel", "And tis", "And this is my resume"],
+            typeSpeed:100,
+            startDelay:100,
+            shuffle:false,
+            backDelay: 500,
+            loop: true,
+            showCursor: false,
+          });
+        });
+});
